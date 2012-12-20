4563d0418c02893a07975941fe3cad19cebb98b4
Vassily Krasnov <mykrasnov@gmail.com>
2012-12-20 12:38:11 +0200

    Some little changes in .js

--- js/resume.js
+++ js/resume.js
@@ -1,5 +1,11 @@
 (function($, undefined) {
 	$(function() {
-		$("p").css("color", "#966");
+	// 	$("p").css("color", "#966");
+		$(".b-header .b-menu .b-menu__item:first").addClass("b-menu__item_state_active");
+		$(".develop").hide();
+
+		$(".b-header .b-menu .b-menu__item").click(function() {
+			
+		});
 	});
 })(jQuery);
